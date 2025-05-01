9d13e058f0409c4b35518a76c0c83a23de214956
mknoufi <mknoufi@gmail.com>
2025-05-01 13:51:19 +0530

    Add files via upload

---
+++ cashiercounter/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Cashier Counter",
+            "color": "#F39C12",
+            "icon": "octicon octicon-briefcase",
+            "type": "module",
+            "label": _("Cashier Counter")
+        }
+    ]
