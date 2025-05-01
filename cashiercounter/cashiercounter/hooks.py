255dac7f3d9b2c050ee1194b39f3a7acd8754fe4
mknoufi <mknoufi@gmail.com>
2025-05-01 07:38:05 +0000

    Moved app scaffold to root for Frappe Cloud compatibility

--- cashiercounter/cashiercounter/hooks.py
+++
@@ -1,7 +0,0 @@
-app_name = "cashiercounter"
-app_title = "Cashier Counter"
-app_publisher = "Lavanya Emart"
-app_description = "Custom cashier module for ERPNext"
-app_email = "your@email.com"
-app_license = "MIT"
-
