718b78c01dd64e9dcfe843dec1dc3748606d0abe
mknoufi <mknoufi@gmail.com>
2025-05-01 07:26:33 +0000

    Completed valid Frappe app scaffold for cashiercounter

---
+++ cashiercounter/cashiercounter/hooks.py
@@ -0,0 +1,7 @@
+app_name = "cashiercounter"
+app_title = "Cashier Counter"
+app_publisher = "Lavanya Emart"
+app_description = "Custom cashier module for ERPNext"
+app_email = "your@email.com"
+app_license = "MIT"
+
